453049f19e93c798282a88bc50f080aa504a1b19
Rio Myers <riomyers@gmail.com>
2018-05-14 20:48:08 -0500

    improved link handling

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ jQuery(function ($) {
 
     'use strict';
     // --------------------------------------------------------------------
-    // jQuery one page scrolling
+    // jQuery One Page Scrolling & Link Handling
     // --------------------------------------------------------------------
 
     (function () {
@@ -20,6 +20,10 @@ jQuery(function ($) {
 		        }
 		    }
 		});
+		// Open all external links in new window 
+		$('a').filter(function () {return this.hostname != window.location.hostname;}).attr('target', '_blank');
+		// No event for empty links
+		$('a[href=#]').on('click', function(){return false;});			
     }());
 
     // --------------------------------------------------------------------
