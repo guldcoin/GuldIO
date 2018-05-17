60f52f21db80d3305432385a66a1debd5149b41a
Rio Myers <riomyers@gmail.com>
2018-05-16 19:38:28 -0500

    cleaned up code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 jQuery(function ($) {
-
     'use strict';
     // --------------------------------------------------------------------
     // jQuery One Page Scrolling & Link Handling
     // --------------------------------------------------------------------
-
-    (function () {
 		// Select all links with hashes
 		$('a[href*=#]:not([href=#])').on('click', function(){
 		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
@@ -20,23 +17,19 @@ jQuery(function ($) {
 		        }
 		    }
 		});
+		
 		// Open all external links in new window 
 		$('a').filter(function () {return this.hostname != window.location.hostname;}).attr('target', '_blank');
+		
 		// No event for empty links
-		$('a[href=#]').on('click', function(){return false;});			
-    }());
-
+		$('a[href=#]').on('click', function(){return false;});	
+			
     // --------------------------------------------------------------------
     // Closes the Responsive Menu on Menu Item Click
     // --------------------------------------------------------------------
-
-    (function () {
-        $('.navbar-collapse ul li a').on('click', function () {
-            if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
-                $('.navbar-toggle:visible').trigger('click');
-            }
-        });
-    }());
+		$('.navbar-nav>li>a,nav a').on('click', function(){
+			$('.navbar-collapse').collapse('hide');
+		});
 
 }); // JQuery end
 
